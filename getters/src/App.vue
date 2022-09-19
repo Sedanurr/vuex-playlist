2575976c9ea98f931221ea246232ecc37c7f4a7d
Sedanur <50722053+Sedanurr@users.noreply.github.com>
2022-09-19 16:16:55 +0300

    tutorial-04

---
+++ getters/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ProductListOne></ProductListOne>
+    <ProductListTwo></ProductListTwo>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/productListOne.vue";
+import ProductListTwo from "./components/productListTwo.vue";
+
+export default {
+  name: "App",
+  components: {
+    ProductListOne,
+    ProductListTwo,
+  },
+};
+</script>
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
